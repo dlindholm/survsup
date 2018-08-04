9fcd9b35c5cc5bd23296f4ec66e97d64f70bae69
Daniel Lindholm <daniel.lindholm@me.com>
2018-08-04 16:27:46 +0200

    minor tweaks for checks

--- R/nar.R
+++ R/nar.R
@@ -1,9 +1,9 @@
 #' Add a numbers at risk table to a Kaplan-Meier plot
 #'
 #' Creates a numbers at risk table based on a ggplot object created by
-#' the plot_survfit function. It will add a table in the lower part of 
+#' the plot_survfit function. It will add a table in the lower part of
 #' the plot, with numbers where the X axis tick marks are situated.
-#' 
+#'
 #' @param g a ggplot object to which a numbers at risk table should be added.
 #' @param size a numeric value indicating the font size of the NAR table
 #' @param x_offset a constant indicating how much padding should be placed to the
@@ -22,35 +22,37 @@
 #' @param sep_lwd what width should that line have?
 #' @param sep_color which color should we use for that line?
 #' @return Returns a ggplot object
-#' 
+#'
 #' @export
 #' @importFrom ggplot2 ggplot_build geom_text geom_hline scale_x_continuous scale_y_continuous
 #' @importFrom dplyr %>% group_by mutate filter arrange slice ungroup select desc
-#' @author Daniel Lindholm 
+#' @author Daniel Lindholm
 
 #' @examples
 #' library(survsup); library(ggplot2); library(dplyr); library(survival)
-#' 
-#' retinopathy %>% 
-#'     survfit(Surv(futime, status) ~ trt, data = .) %>% 
-#'	   plot_survfit() %>% 
+#'
+#' retinopathy %>%
+#'     survfit(Surv(futime, status) ~ trt, data = .) %>%
+#'	   plot_survfit() %>%
 #' 	   nar()
-#' 
+#'
 #' # Without table flip:
-#' flchain %>% 
-#'     survfit(Surv(futime, death) ~ sex, data = .) %>% 
-#'	   plot_survfit(cuminc = FALSE) %>% 
+#' flchain %>%
+#'     survfit(Surv(futime, death) ~ sex, data = .) %>%
+#'	   plot_survfit(cuminc = FALSE) %>%
 #'	   nar()
 #'
 #' # With table flip:
-#' flchain %>% 
-#'     survfit(Surv(futime, death) ~ sex, data = .) %>% 
-#'	   plot_survfit(cuminc = FALSE) %>% 
+#' flchain %>%
+#'     survfit(Surv(futime, death) ~ sex, data = .) %>%
+#'	   plot_survfit(cuminc = FALSE) %>%
 #'	   nar(flip = TRUE)
 
 
 nar <- function(g, size = NA, x_offset = 0.02, y_offset = 0.05, forced_height = NULL, flip = FALSE, separator = TRUE, sep_lwd = 0.20, sep_color = "grey70"){
 	stopifnot("ggplot" %in% class(g))
+
+strata <- time <- n.risk <- max_time <- x <- y <- NULL
 
 	# Get X axis range and ticks
 	x_range <- ggplot_build(g)$layout$panel_params[[1]]$x.range
@@ -61,7 +63,7 @@ nar <- function(g, size = NA, x_offset = 0.02, y_offset = 0.05, forced_height = 
 	y_range <- ggplot_build(g)$layout$panel_params[[1]]$y.range
 	y_size <- y_range[2]-y_range[1]
 	y_values <- ggplot_build(g)$layout$panel_params[[1]]$y.major_source
-	
+
 	# Get data from ggplot object
 	d <- g$data
 
@@ -72,18 +74,18 @@ nar <- function(g, size = NA, x_offset = 0.02, y_offset = 0.05, forced_height = 
 		nar <- data.frame(n.risk = double(), strata = character(), x = integer(), stringsAsFactors = FALSE)
 
 		for(i in x_values){
-			tmp <- d %>% group_by(strata) %>% 
+			tmp <- d %>% group_by(strata) %>%
 				mutate(max_time = max(time)) %>%
-				filter(time <= i) %>% 
-				filter(time == max(time) ) %>% 
-				arrange(desc(n.risk)) %>% 
-				slice(1) %>% 
+				filter(time <= i) %>%
+				filter(time == max(time) ) %>%
+				arrange(desc(n.risk)) %>%
+				slice(1) %>%
 				mutate(n.risk = ifelse(i > max_time, 0L, n.risk)) %>%
-				ungroup() %>% 
-				select(n.risk, strata) %>% 
+				ungroup() %>%
+				select(n.risk, strata) %>%
 				mutate(x = i)
 			nar <- bind_rows(tmp, nar)
-		}	
+		}
 
 		# Get Y values for the NAR table
 		nar$y <- as.double(NA)
@@ -92,7 +94,7 @@ nar <- function(g, size = NA, x_offset = 0.02, y_offset = 0.05, forced_height = 
 			if(!flip) nar[nar$strata == rev(unique(nar$strata))[i],]$y <- y_range[1] - (i * y_offset * y_size) else
 			nar[nar$strata == unique(nar$strata)[i],]$y <- y_range[1] - (i * y_offset * y_size)
 		}
-	
+
 	} else {
 		#### Create numbers at risk table in case there are _no_ strata
 
@@ -100,24 +102,24 @@ nar <- function(g, size = NA, x_offset = 0.02, y_offset = 0.05, forced_height = 
 		nar <- data.frame(n.risk = double(), x = integer())
 
 		for(i in x_values){
-			tmp <- d %>% 
+			tmp <- d %>%
 				mutate(max_time = max(time)) %>%
-				filter(time <= i) %>% 
-				filter(time == max(time) ) %>% 
-				arrange(desc(n.risk)) %>% 
-				slice(1) %>% 
-				mutate(n.risk = ifelse(i > max_time, 0L, n.risk)) %>% 
-				select(n.risk) %>% 
+				filter(time <= i) %>%
+				filter(time == max(time) ) %>%
+				arrange(desc(n.risk)) %>%
+				slice(1) %>%
+				mutate(n.risk = ifelse(i > max_time, 0L, n.risk)) %>%
+				select(n.risk) %>%
 				mutate(x = i)
 			nar <- bind_rows(tmp, nar)
-		}	
+		}
 
 		# Get Y values for the NAR table
 		nar$y <- as.double(NA)
 		nar$y <- y_range[1] - (y_offset * y_size)
 
-	}	
-	
+	}
+
 	# Implement the forced_height option
 	if(!is.null(forced_height[1])){
 		stopifnot(is.numeric(forced_height))
@@ -137,7 +139,7 @@ nar <- function(g, size = NA, x_offset = 0.02, y_offset = 0.05, forced_height = 
 			old_y <- unique(nar$y)
 			old_y <- old_y[order(old_y)]
 			for(i in 1:length(old_y)){
-				nar[nar$y == old_y[i], ]$y <- new_y[i] 
+				nar[nar$y == old_y[i], ]$y <- new_y[i]
 			}
 		} else {
 			# Update Y to center NAR row (no strata) within this space
@@ -148,16 +150,16 @@ nar <- function(g, size = NA, x_offset = 0.02, y_offset = 0.05, forced_height = 
 
 	}
 	# Add NAR table, and make sure that Y ticks are unchanged
-	
+
 	if(!is.null(forced_height[1])){
 		suppressMessages(
-			g <- g + geom_text(data=nar, mapping = aes(x = x, y = y, label = n.risk), show.legend = FALSE, size = size)+ 
+			g <- g + geom_text(data=nar, mapping = aes(x = x, y = y, label = n.risk), show.legend = FALSE, size = size)+
 			scale_y_continuous(breaks = y_values, limits = c(nar_range[1], y_range[2]))+
 			scale_x_continuous(breaks = x_values, limits = c(x_range[1] - x_offset * x_size, x_range[2]))
 		)
 	} else {
 		suppressMessages(
-			g <- g + geom_text(data=nar, mapping = aes(x = x, y = y, label = n.risk), show.legend = FALSE, size = size)+ 
+			g <- g + geom_text(data=nar, mapping = aes(x = x, y = y, label = n.risk), show.legend = FALSE, size = size)+
 			scale_y_continuous(breaks = y_values, limits = c(min(nar$y), y_range[2]))+
 			scale_x_continuous(breaks = x_values, limits = c(x_range[1] - x_offset * x_size, x_range[2]))
 		)
--- R/plot_survfit.R
+++ R/plot_survfit.R
@@ -1,11 +1,11 @@
 #' Plot a survfit object
 #'
 #' Creates a Kaplan-Meier plot based on a survfit object
-#' 
+#'
 #' @param fit A survfit object that we should plot
 #' @param lwd Line width
 #' @param xmax What is the upper limit of the X axis? The default will plot all data.
-#' @param xbreaks A numeric vector. Where should the breaks in the X axis be? The default will give ggplot's 
+#' @param xbreaks A numeric vector. Where should the breaks in the X axis be? The default will give ggplot's
 #'     default breaks.
 #' @param ylim A numeric vector of length 2. Which are the Y axis limits?
 #' @param ci A logical scalar. Should confidence intervals be plotted?
@@ -18,45 +18,45 @@
 #'     was used; otherwise it will take the value "strata"
 #' @param legend.position Where should the legend be drawn? Possible answers are: "top", "bottom",
 #'     "left", "right"; or if you want to skip putting a legend on the plot: "none"
-#' 
+#'
 #' @return Returns a ggplot object
-#' @importFrom ggplot2 ggplot aes geom_ribbon geom_step guides scale_x_continuous ylim theme
-#' @importFrom dplyr bind_rows %>% mutate 
+#' @importFrom ggplot2 ggplot aes geom_ribbon geom_step guides guide_legend scale_x_continuous ylim theme
+#' @importFrom dplyr bind_rows %>% mutate
 #' @export
 
-#'  
-#' @author Daniel Lindholm 
+#'
+#' @author Daniel Lindholm
 #' @examples
-#' 
+#'
 #' library(survsup); library(ggplot2); library(dplyr); library(survival)
-#' 
-#' retinopathy %>% 
-#'     survfit(Surv(futime, status) ~ trt, data = .) %>% 
+#'
+#' retinopathy %>%
+#'     survfit(Surv(futime, status) ~ trt, data = .) %>%
 #'	   plot_survfit()
-#' 
-#' retinopathy %>% 
-#'     survfit(Surv(futime, status) ~ trt, data = .) %>% 
+#'
+#' retinopathy %>%
+#'     survfit(Surv(futime, status) ~ trt, data = .) %>%
 #'	   plot_survfit(ci = TRUE, cuminc = FALSE)
-#' 
+#'
 #' # Since a ggplot object is returned,
 #' # you can tweak as you wish
-#' # (in this case with multiple labels): 
-#' mgus2 %>% 
-#'      survfit(Surv(futime, death) ~ sex, data = .) %>% 
+#' # (in this case with multiple labels):
+#' mgus2 %>%
+#'      survfit(Surv(futime, death) ~ sex, data = .) %>%
 #'      plot_survfit(cuminc = FALSE) %>%
-#'      nar(flip = TRUE) %>% 
+#'      nar(flip = TRUE) %>%
 #'      hcl_rainbow()+  # <--- Notice + sign here!
 #'      labs(title = "This is an excessively long title",
 #'      	subtitle = "This is a subtitle",
 #'      	x = "Time (days)",
 #'      	y = "Survival (%)",
 #'      	caption = "(This is a demo)")
- 
+
 
 
 plot_survfit <- function(fit, lwd = 1, xmax = NULL, xbreaks = NULL, ylim = NULL, ci = FALSE, cuminc = TRUE, y_percent = TRUE,
 	split_legend_labels = TRUE, legend.title = NA, legend.position="top"){
-	
+
 	#Checks
 	stopifnot("survfit" %in% class(fit))
 	stopifnot(is.logical(cuminc))
@@ -64,13 +64,15 @@ plot_survfit <- function(fit, lwd = 1, xmax = NULL, xbreaks = NULL, ylim = NULL,
 	stopifnot(is.logical(y_percent))
 	stopifnot(is.logical(split_legend_labels))
 	stopifnot(legend.position %in% c("top", "bottom", "left", "right", "none"))
-	
+
+estimate <- conf.high <- conf.low <- time <- strata <- NULL
+
 	#Tidy fit to clean data.frame
 	tidy_fit <- broom::tidy(fit)
 
 	#Make sure to start from 1
 	if(exists("strata", fit)){
-		
+
 		baseline <- data.frame(time = 0,
 							   n.risk = fit$n,
 							   n.event = 0,
@@ -82,9 +84,9 @@ plot_survfit <- function(fit, lwd = 1, xmax = NULL, xbreaks = NULL, ylim = NULL,
 							   strata = names(fit$strata),
 							   stringsAsFactors = FALSE)
 		tidy_fit <- bind_rows(tidy_fit, baseline)
-	
+
 	} else {
-	
+
 		baseline <- data.frame(time = 0,
 							   n.risk = fit$n,
 							   n.event = 0,
@@ -95,25 +97,25 @@ plot_survfit <- function(fit, lwd = 1, xmax = NULL, xbreaks = NULL, ylim = NULL,
 							   conf.low = 1,
 							   stringsAsFactors = FALSE)
 		tidy_fit <- bind_rows(tidy_fit, baseline)
-	
+
 	}
 
 	#Transform if desired
 	if(cuminc){
-	
-		tidy_fit <- tidy_fit %>% 
+
+		tidy_fit <- tidy_fit %>%
 			mutate(estimate = 1 - estimate, conf.high = 1 - conf.high, conf.low = 1 - conf.low)
-	
+
 	}
 
 	#Multiply y with 100 if desired
 	if(y_percent){
 
-		tidy_fit <- tidy_fit  %>% 
-			mutate(estimate = 100 * estimate, 
+		tidy_fit <- tidy_fit  %>%
+			mutate(estimate = 100 * estimate,
 				conf.high = 100 * conf.high,
 				conf.low = 100 * conf.low)
-	
+
 	}
 
 	# Split labels if desired
@@ -132,16 +134,16 @@ plot_survfit <- function(fit, lwd = 1, xmax = NULL, xbreaks = NULL, ylim = NULL,
 		if(!is.na(legend.title)) strata_label <- legend.title
 
 		if(ci){
-			
+
 			res <- ggplot(tidy_fit, aes(x = time, y = estimate, color = strata))+
 				geom_ribbon(show.legend = FALSE, fill = "grey60", alpha = 0.2, lty = 0, mapping = aes(ymin = conf.low, ymax = conf.high))+
 				geom_step(lwd=lwd)
-		
+
 		} else {
-		
+
 			res <- ggplot(tidy_fit, aes(x = time, y = estimate, color = strata))+
 				geom_step(lwd=lwd)
-		
+
 		}
 
 			if(exists("strata_label")) res <- res + guides(color = guide_legend(title = strata_label) )
@@ -150,16 +152,16 @@ plot_survfit <- function(fit, lwd = 1, xmax = NULL, xbreaks = NULL, ylim = NULL,
 	} else {
 
 		if(ci){
-		
+
 			res <- ggplot(tidy_fit, aes(x = time, y = estimate))+
 				geom_ribbon(show.legend = FALSE, fill = "grey60", alpha = 0.2, lty = 0, mapping = aes(ymin = conf.low, ymax = conf.high))+
 				geom_step(lwd=lwd)
-		
+
 		} else {
-		
+
 			res <- ggplot(tidy_fit, aes(x = time, y = estimate)) + geom_step(lwd=lwd)
-		
-		}	
+
+		}
 	}
 
 	# Create custom x limit and x breaks if specified
@@ -167,23 +169,23 @@ plot_survfit <- function(fit, lwd = 1, xmax = NULL, xbreaks = NULL, ylim = NULL,
 		if(!is.null(xmax[1])){
 
 			res <- res + scale_x_continuous(breaks = xbreaks, limits = c(0, xmax))
-		
+
 		} else res <- res + scale_x_continuous(breaks = xbreaks)
-	} 
+	}
 
 	else {
-		
+
 		if(!is.null(xmax[1])){
-			
+
 			res <- res + scale_x_continuous(limits = c(0, xmax))
-		
+
 		}
 	}
 
 	# Create custom y limits if desired
 	if(!is.null(ylim[1])){
 		res <- res + ylim(ylim)
-	}	
+	}
 
 
 	# Put legend where desired
